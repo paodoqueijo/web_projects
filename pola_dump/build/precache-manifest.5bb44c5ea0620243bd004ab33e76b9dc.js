7fb443abbcd27f4fe4a2d1c75943d9d20c75cd9e
michael_sattler <sattlermichi@gmail.com>
2020-11-05 22:42:33 +0100

    changed folder structure in pola_dump

---
+++ pola_dump/build/precache-manifest.5bb44c5ea0620243bd004ab33e76b9dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb50a9ff017d5307c76f90ad0dad816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b2a119cdcbb9f2b57e",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "73e7c2011392d1f221cc",
+    "url": "/static/css/main.6b7e308a.chunk.css"
+  },
+  {
+    "revision": "c8b2a119cdcbb9f2b57e",
+    "url": "/static/js/2.79c716ab.chunk.js"
+  },
+  {
+    "revision": "26583a9f0388b3dd2e07bff0dda5ec34",
+    "url": "/static/js/2.79c716ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e7c2011392d1f221cc",
+    "url": "/static/js/main.88b52f8b.chunk.js"
+  },
+  {
+    "revision": "82e381653e1c95364381",
+    "url": "/static/js/runtime-main.38c03c00.js"
+  },
+  {
+    "revision": "c49471139fc4d00cb680cc85b81a84c1",
+    "url": "/static/media/camera.c4947113.svg"
+  }
+]);
